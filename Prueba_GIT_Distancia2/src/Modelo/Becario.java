fb3f06bbbf6211527f8ee9b5d889997c8b16db2a
menen <menen@DESKTOP-N1DTRB2>
2023-12-17 19:14:54 +0100

    Primer commit de la rama becario

---
+++ Prueba_GIT_Distancia2/src/Modelo/Becario.java
@@ -0,0 +1,11 @@
+package Modelo;
+
+public class Becario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("trabajo duro del becario");
+
+	}
+
+}
