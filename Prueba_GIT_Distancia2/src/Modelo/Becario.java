0515b75664b60e9341fbdbf201b35c7612cbbb43
menen <menen@DESKTOP-N1DTRB2>
2023-12-17 19:21:02 +0100

    Segundo commit en la rama becario

--- Prueba_GIT_Distancia2/src/Modelo/Becario.java
+++ Prueba_GIT_Distancia2/src/Modelo/Becario.java
@@ -5,6 +5,8 @@ public class Becario {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("trabajo duro del becario");
+		
+		System.out.println("segundo dia de trabajo duro de becario");
 
 	}
 
