29b0092ce06e6a1e37f4f1ec1d48cab721f45f60
menen <menen@DESKTOP-N1DTRB2>
2023-12-17 11:41:18 +0100

    Interfaces y modelo

---
+++ Prueba_GIT_Distancia2/src/Modelo/Miclase.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+import interfaces.Miinterfaz;
+
+public class Miclase implements Miinterfaz{
+
+	@Override
+	public int metododevuelveentero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
