2dcab0f620d098fd1819b0f35610888588ccf937
Yoana16 <154019199+Yoana16@users.noreply.github.com>
2023-12-17 16:55:59 +0100

    Update domingo2.java

--- Prueba_GIT_Distancia2/src/Modelo/Miclase.java
+++ Prueba_GIT_Distancia2/src/Modelo/Miclase.java
@@ -7,6 +7,7 @@ public class Miclase implements Miinterfaz{
 	@Override
 	public int metododevuelveentero() {
 		// TODO Auto-generated method stub
+		// no me dio tiempo de implementar este metodo
 		return 0;
 	}
 
